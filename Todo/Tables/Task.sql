6135bf277736bac8036e996961928d43a557cf73
Jakub Prusak <Jakub Prusak>
2015-11-07 17:04:19 +0100

    initial

---
+++ Todo/Tables/Task.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Task]
+(
+	[TaskId] INT NOT NULL  IDENTITY, 
+    [Name] NVARCHAR(1000) NOT NULL, 
+    [DateAdd] DATETIME NOT NULL DEFAULT getdate(), 
+    [UserAddId] INT NOT NULL, 
+    [UserId] INT NOT NULL, 
+    CONSTRAINT [PK_Task] PRIMARY KEY ([TaskId])
+)
